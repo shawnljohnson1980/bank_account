7c8cef76f672513e61c3779bcf6e7a998883d13a
shawnljohnson1980 <78113657+shawnljohnson1980@users.noreply.github.com>
2021-04-07 17:41:36 -0400

    file update

--- bank_account.py
+++ bank_account.py
@@ -1,16 +1,10 @@
-class User:
-    def __init__(self, name_input, email_input):
-        self.name=name_input
-        self.email=email_input
-        self.account_balance=0.0
+
         
-class Bank_account:
+
+class Bank_Account:
     def __init__(self, int_rate,account_balance,name_input,email_input):
      self.name=name_input
      self.email=email_input
-     self. account_balance=0.00
-     self.int_rate=0.1
-     
     def add_interest(self):
          self.account_balance=(self.accout_balance*self.account_in_rate)
          print(self.account_balance)
@@ -28,5 +22,18 @@ class Bank_account:
     def make_widthdrawl(self, amount):
         self.account_balance -= amount
         return self
-        
+class User:
+    def __init__(self, name_input, email_input):
+        self.name=name_input
+        self.email=email_input
+        self.account = Bank_Account(int_rate=0.01, balance=0)
 
+shawn = User("Shawn","shawn@mail.com")
+andrew= User ("Andrew","andrew@mail.com")    
+
+shawn.account.make_deposit(700).venmo(500, andrew).make_withdrawl(50)
+print(f"Shawn's Account balance: ${shawn.account.balance} ")
+print(f"Andrews account balance: ${andrew.balance}")
+shawn.make_deposit(700).venmo(500, andrew)
+print(f"Shawn's Account balance: ${shawn.account.balance} ")
+print(f"Andrews account balance: ${andrew.account.balance}")
